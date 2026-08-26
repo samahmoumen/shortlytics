1ee11a8ec6c29fd4105c01e172f762a697120783
samahmoumen <samahmn98@gmail.com>
2026-08-26 22:18:23 +0200

    added flyway configuration

---
+++ backend/src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,27 @@
+-- 1. Table users
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255),
+    username VARCHAR(255),
+    password VARCHAR(255),
+    role VARCHAR(255) DEFAULT 'ROLE_USER'
+);
+
+-- 2. Table url_mapping
+CREATE TABLE url_mapping (
+    id BIGSERIAL PRIMARY KEY,
+    original_url VARCHAR(255),
+    short_url VARCHAR(255),
+    click_count INT NOT NULL DEFAULT 0,
+    created_date TIMESTAMP,
+    user_id BIGINT,
+    CONSTRAINT fk_url_mapping_user FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- 3. Table click_event
+CREATE TABLE click_event (
+    id BIGSERIAL PRIMARY KEY,
+    click_date TIMESTAMP,
+    url_mapping_id BIGINT,
+    CONSTRAINT fk_click_event_url_mapping FOREIGN KEY (url_mapping_id) REFERENCES url_mapping (id)
+);
